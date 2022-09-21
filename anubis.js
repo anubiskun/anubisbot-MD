c7000b93f53b4bc98e8d362ff31b8659d8223c4b
anubiskun <adivian24@gmail.com>
2022-09-21 10:06:51 +0700

    lupa push

--- anubis.js
+++ anubis.js
@@ -521,17 +521,20 @@ function startAnubis() {
      * @param {*} options 
      * @returns 
      */
-    anubis.sendImageAsSticker = async (jid, path, quoted, options = {}) => {
-        let buff = Buffer.isBuffer(path) ? path : /^data:.*?\/.*?;base64,/i.test(path) ? Buffer.from(path.split`,`[1], 'base64') : /^https?:\/\//.test(path) ? await (await getBuffer(path)) : fs.existsSync(path) ? fs.readFileSync(path) : Buffer.alloc(0)
-        let buffer
-        if (options && (options.packname || options.author)) {
-            buffer = await writeExifImg(buff, options)
-        } else {
-            buffer = await imageToWebp(buff)
+     anubis.sendAsSticker = async (jid, path, quoted, options = {}) => {
+        try {
+            let buff = Buffer.isBuffer(path) ? path : /^data:.*?\/.*?;base64,/i.test(path) ? Buffer.from(path.split`,`[1], 'base64') : /^https?:\/\//.test(path) ? await (await getBuffer(path)) : fs.existsSync(path) ? fs.readFileSync(path) : Buffer.alloc(0)
+            let buffer
+            if (options && (options.packname || options.author)) {
+                buffer = await writeExif(buff, options)
+            } else {
+                buffer = await imageToWebp(buff)
+            }
+            await anubis.sendMessage(jid, { sticker: { url: buffer }, ...options }, { quoted })
+            return buffer
+        } catch (err) {
+            console.log(err)
         }
-
-        await anubis.sendMessage(jid, { sticker: { url: buffer }, ...options }, { quoted })
-        return buffer
     }
 
     /**
