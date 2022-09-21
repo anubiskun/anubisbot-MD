949d0f7d907b4951cb4abe0b6efb5bf258361832
anubiskun <adivian24@gmail.com>
2022-09-21 09:45:40 +0700

    fix

--- plugins/public.js
+++ plugins/public.js
@@ -1,23 +1,14 @@
 module.exports = anuplug = async(m, { anubis, text, command, args, usedPrefix, botNumber }) => {
     let public = botpublic
-    console.log(public)
     let pubMes
     if (public) pubMes = 'public nyala ngab!'
     if (!public) pubMes = 'public mati ngab!'
-    if (!text) return anubis.send5ButMsg(m.chat, 'Pilih untuk switch mode bot!\n\n'+ pubMes, '', [
-        {
-            quickReplyButton: {
-                displayText: "ON",
-                id: `${usedPrefix}public on`,
-            }
-        },
-        {
-            quickReplyButton: {
-                displayText: "OFF",
-                id: `${usedPrefix}public off`,
-            },
-        }
-    ])
+    let buttons = [
+        { buttonId: `${usedPrefix}public on`, buttonText: { displayText: "ON" }, type: 1 },
+        { buttonId: `${usedPrefix}public off`, buttonText: { displayText: "OFF" }, type: 1 },
+      ];
+    let buttonMessage = { text: 'Pilih untuk switch mode bot\n\n' + pubMes, footer: global.anuFooter, buttons };
+    if (!text) return anubis.sendMessage(m.chat, buttonMessage, { quoted: m });
     if (text == 'on') {
         botpublic = true
         anubis.sendMessage(m.chat, {text: 'Bot sekarang bisa merespons semua orang!'}, {quoted: m})
