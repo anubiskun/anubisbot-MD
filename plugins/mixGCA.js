d63dcf8328cc0eed33f8d8ad4a4b85fb1a45e9fa
Kanna-Chan <77791484+irham01@users.noreply.github.com>
2022-10-12 13:09:26 +0700

    Update mixGCA.js

--- plugins/mixGCA.js
+++ plugins/mixGCA.js
@@ -9,6 +9,12 @@
 
  module.exports = anuplug = async(m, anubis, { participants, text, command, args, usedPrefix }) => {
      let vote = anubis.db.data.others.vote;
+     let user = m.mentionedJid[0]
+             ? m.mentionedJid[0]
+             : m.quoted
+             ? m.quoted.sender
+             : text.replace(/[^0-9]/g, "");
+     let users = user.split("@")[0]
      switch(command){
          case 'add':
              {
@@ -28,17 +34,57 @@
          break;
          case 'kick':
              {
-                 let user = m.mentionedJid[0]
-                         ? m.mentionedJid[0]
-                         : m.quoted
-                         ? m.quoted.sender
-                         : text.replace(/[^0-9]/g, "");
-                 let users = user.split("@")[0]
                  let response = await anubis.groupParticipantsUpdate(m.chat, [users + anubis.anubiskun], "remove")
                  if (response[0].status == 200) return m.reply('Berhasil mengeluarkan')
                  else return m.reply('Gagal mengeluarkan ngab!\nNomer tidak ada di group!')
              }
          break;
+        case 'promote':
+            {
+                let response = await anubis.groupParticipantsUpdate(m.chat, [users + anubis.anubiskun], "promote")
+                if (response[0].status == 200) return m.reply('Berhasil promote admin')
+                else m.reply('Gagal mengeluarkan ngab!\nNomer tidak ada di group!')
+            }
+        break;
+        case 'demote':
+            {
+                let response = await anubis.groupParticipantsUpdate(m.chat, [users + anubis.anubiskun], "demote")
+                if (response[0].status == 200) return m.reply('Berhasil demote admin')
+                else m.reply('Gagal mengeluarkan ngab!\nNomer tidak ada di group!')
+            }
+        break;
+        case 'setname':
+            {
+                if (!text) return m.reply(`example : ${usedPrefix + command} KannaBOT Official`)
+                await anubis.groupUpdateSubject(m.chat, text).then((res) => m.reply('Berhasil mengubah nama grup')).catch((err) => m.reply('Gagal mengubah nama grup'))
+            }
+        break;
+        case 'setdesk': case 'setdeskripsi':
+            {
+                if (!text) return m.reply(`example : ${usedPrefix + command} Rules`)
+                await anubis.groupUpdateDescription(m.chat, text).then((res) => m.reply('Berhasil mengubah deskripsi grup')).catch((err) => m.reply('Gagal mengubah deskripsi grup'))
+            }
+        break;
+        case 'setppgroup': case 'setppgc': case 'setppgrup':
+            {
+                const mquo = m.quoted || m;
+                const quoted = mquo.mtype == "buttonsMessage"
+                    ? mquo[Object.keys(mquo)[1]]
+                    : mquo.mtype == "templateMessage"
+                    ? mquo.hydratedTemplate[Object.keys(mquo.hydratedTemplate)[1]]
+                    : mquo.mtype == "product"
+                    ? mquo[Object.keys(mquo)[0]]
+                    : m.quoted
+                    ? m.quoted
+                    : m;
+                const mime = (quoted.msg || quoted).mimetype || "";
+                const qmsg = quoted.msg || quoted;
+                if (!/(webp|image)/.test(mime)) return m.reply(`Kirim/reply image dengan caption ${usedPrefix + command}`)
+                let media = await anubis.downloadAndSaveMediaMessage(qmsg);
+                await anubis.updateProfilePicture(m.chat, { url: media }).catch((err) => fs.unlinkSync(media))
+                m.reply('Berhasil mengubah poto profile grup')
+            }
+        break;
          case 'del':
          case 'delete':
              {
@@ -100,7 +146,7 @@
  }
  anuplug.help = ['add', 'kick', 'delete','vote','delvote']
  anuplug.tags = ['group']
- anuplug.command = /^(add|kick|del(ete)?|(del)?vote)$/i
+ anuplug.command = /^(add|kick|(pro|de)mote|set(desk(ripsi)?|name|pp(gc|group|grup))|del(ete|vote)?)$/i
  anuplug.group = true
  anuplug.botAdmin = true
  anuplug.admin = true
